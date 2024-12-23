96b843c96178ba130c0158551eab268c27f918cc
Sama-E <112719147+Sama-E@users.noreply.github.com>
2024-12-22 21:57:11 -0500

    frontend layout

--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -1,20 +1,46 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect } from 'react'
 import Navbar from './(components)/Navbar'
 import Sidebar from './(components)/Sidebar'
+import StoreProvider, { useAppSelector } from './redux'
 
-const DashboardWrapper = ({ children }: {children:React.ReactNode}) => {
+const DashboardLayout = ({ children }: {children:React.ReactNode}) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
         {/* SIDEBAR */}
         <Sidebar />
         {/* MAIN */}
-        <main className={`flex w-full flex-col bg-gray-50 md:pl-64`}>
-            {/* NAVBAR */}
-            <Navbar />
-            {children}
+        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+          isSidebarCollapsed ? "" : "md:pl-64"}`} 
+        >
+          {/* NAVBAR */}
+          <Navbar />
+          {children}
         </main>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardWrapper
+const DashboardWrapper = ({ children }: {children:React.ReactNode}) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
+
+export default DashboardWrapper;
